ecdeca3707ca29cb25150b133cdd9fc6a52f65b5
nganty237 <surflinkayos@gmail.com>
2025-11-23 19:22:22 +0100

    icons modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Form from "./Form";
 function App() {
   return (
-    <div className="bg-gray-50 flex items-center justify-center min-h-screen p-4">
+    <div className="bg-gray-50 flex items-center justify-center min-h-screen ">
       <Form />
     </div>
   );
